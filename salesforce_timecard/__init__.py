5b1a03874eef0f313c7c0f0a6c2b0a129e3b0975
Giulio Calzolari <giuliocalzolari@users.noreply.github.com>
2019-08-21 14:14:23 +0200

    bump 0.3.7

--- salesforce_timecard/__init__.py
+++ salesforce_timecard/__init__.py
@@ -1,7 +1,7 @@
 __title__ = 'salesforce-timecard'
 __description__ = 'PSE Timecard CLI'
 __url__ = 'https://github.com/giuliocalzolari/salesforce-timecard'
-__version__ = '0.3.6'
+__version__ = '0.3.7'
 __author__ = 'giuliocalzolari'
 __author_email__ = 'giuliocalzolari@users.noreply.github.com'
 __license__ = 'MIT'
